958bb55ff1f20d3c5ed3ee7e1ff10732a63af797
Piyush Aryan <piyusharyan81@gmail.com>
2024-01-30 01:47:00 +0530

    animate vector image

--- src/components/panel.js
+++ src/components/panel.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const panel = () => {
+const Panel = () => {
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -14,14 +14,14 @@ const panel = () => {
 
   return (
     <div className={`flex items-center justify-center h-screen transition-transform ${loaded ? 'translate-x-0' : '-translate-x-full'}`}>
-      <h1 className="font-sans-serif text-5xl text-teal-700 pl-10 pb-10 font-black text-left">Empower Your Data, Unleash
+      <h1 className="font-sans text-5xl text-teal-700 pl-10 pb-10 font-black text-left">Empower Your Data, Unleash
         <br />
         <span className="text-6xl text-start text-red-500">A</span>
         <span className="text-6xl text-start text-yellow-600">th</span>
         <span className='text-6xl text-start text-blue-700'>ena</span>
         <br />
         Web3 Cloud Storage
-        <div className='text-sm text-black'>Store, share and access all your files in one platform</div>
+        <div className='text-sm text-opacity-80 text-gray-900'>Store, share and access all your files in one platform</div>
         </h1>
         
     </div>
@@ -29,4 +29,4 @@ const panel = () => {
   );
 };
 
-export default panel
+export default Panel
