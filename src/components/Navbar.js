cda266b76a6b35d446002f769967c02e207a6ff4
Piyush Aryan <piyusharyan81@gmail.com>
2024-02-01 12:08:42 +0530

    font change and link added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const nav = [
   { name: 'Home', href: '#', current: true },
@@ -34,11 +35,11 @@ export default function Example() {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
+                  <Link href='#'><img
                     className="h-10 w-auto"
                     src="athena-logo.svg"
                     alt="athena-logo"
-                  />
+                  /></Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
--- src/components/panel.js
+++ src/components/panel.js
@@ -24,7 +24,7 @@ const Panel = () => {
         <br />
         Web3 Cloud Storage
         <br />
-        <span className='border border-current text-sm bg-green-100 font-Inter text-opacity-80 text-gray-900 font-face'>Store, share and access all your files in one platform</span>
+        <span className='border border-transparent text-sm bg-teal-50 font-Inter text-opacity-80 text-gray-900 font-face'>Store, share and access all your files in one platform</span>
         <br />
         <button type='button' className='text-teal-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800
           dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2'> <img src='metamask.svg' alt='metamask' className='w-6 h-5 me-2 ms-1 aria-hidden:' /> Connect with MetaMask
