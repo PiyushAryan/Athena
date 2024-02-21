b93f18b0a3397fcf2bf1a691c8139b4774ec2c21
Piyush Aryan <piyusharyan81@gmail.com>
2024-02-21 11:04:53 +0530

    alignment adjusted in text in panel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,94 +1,29 @@
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link'
+import React, { useState } from "react";
+import { Menu, MenuItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+import Image from "next/image";
 
-const nav = [
-  { name: 'Home', href: '', current: true },
-  { name: 'Contact', href: '/contact', current: false },
-  { name: 'About', href: '/about', current: false },
-]
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
-
-export default function Example() {
+function Navbar({ className }) {
+  const [active, setActive] = useState(null);
   return (
-    <Disclosure as="nav" className="bg-gray-100">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-500 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <Link href='#'><img
-                    className="h-10 w-auto"
-                    src="athena-logo.svg"
-                    alt="athena-logo"
-                  /></Link>
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4 font-Montserrat">
-                    {nav.map((item) => (
-                      <Link
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 font-Montserrat text-white' : 'text-gray-800 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}>
-                        {item.name}
-                      </Link>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {nav.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-700 text-white' : 'text-gray-500 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  )
+    <div className={cn("fixed top-10 inset-x-1 max-w-2xl mx-auto z-50 font-Montserrat", className)}>
+      <Menu setActive={setActive}>
+      <Link href="/">
+        <Image src="/athena-logo.svg" alt="Logo" width={80} height={80} />
+        </Link>
+        <Link href="/">
+          <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+        </Link>
+        <Link href="/about">
+          <MenuItem setActive={setActive} active={active} item="About"></MenuItem>
+        </Link>
+        <Link href="/contact">
+          <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
 }
+
+export default Navbar;
--- src/components/panel.js
+++ src/components/panel.js
@@ -9,19 +9,18 @@ import {
 
 function Panel() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <>
+    <div className="flex items-center text-center h-screen">
       <div className="font-Montserrat text-5xl text-teal-700 ml-10 mb-12 font-black text-left">
         <TextRevealCard
           text="Empower Your Data, Unleash"
           revealText="Athena - Web3 Cloud Storage" />
-        <p className="border ml-10 mb-2 max-w-[40vw] border-transparent text-sm bg-black font-Inter text-opacity-80 text-gray-100 font-face">Store, share and access all your files in one platform</p>
-        <p className="text-sm ml-10 font-Inter bg-gradient-to-l from-red-500 via-blue-600 to-yellow-700 text-transparent bg-clip-text max-w-[50vw]"> Athena offers decentralized cloud storage powered by Web3, ensuring security, privacy, and accessibility. Data is distributed across a network, eliminating reliance on centralized servers. With end-to-end encryption and community governance, users retain control over their data. Experience Athena for secure, decentralized storage solutions in the Web3 era.</p>
+        <p className="bg-transparent ml-10 mb-2 max-w-[40vw] text-sm bg-black font-Inter text-opacity-80 text-gray-100 font-face">Store, share and access all your files in one platform</p>
+        <p className="text-sm ml-10 text-justify font-Inter bg-gradient-to-l from-red-500 via-blue-600 to-yellow-700 text-transparent bg-clip-text max-w-[50vw]"> Athena offers decentralized cloud storage powered by Web3, ensuring security, privacy, and accessibility. Data is distributed across a network, eliminating reliance on centralized servers. With end-to-end encryption and community governance, users retain control over their data. Experience Athena for secure, decentralized storage solutions in the Web3 era.</p>
         <br />
-        {/* <button type="button" className="text-teal-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2">
-          <img src="metamask.svg" alt="metamask" className="w-6 h-5 me-2 ms-1 aria-hidden:" /> Connect with MetaMask
-        </button> */}
       </div>
     </div>
+    </>
   );
 }
 
